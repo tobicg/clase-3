90ec1bd3e994c35b146336ca1c5b365c420f48fc
tobicg <tobicareaga@gmail.com>
2021-10-24 03:18:04 -0300

    clase-3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
-function App() {
+function App() { //componente tipo funcion
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundColor: '#303030', width: '100%', height: '100vh'}}>
+      <NavBar />
     </div>
   );
 }
+
+//no usamos mas CLASS, ahora usamos className
 
 export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,20 @@
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <div className="navbar">
+            <div className="navbar-container">
+                <h1 className="navbar-container-title">CA BURGERS</h1>
+                <ul className="navbar-container-components">
+                    <li className="navbar-container-components-items"><a href="">Inicio</a></li>
+                    <li className="navbar-container-components-items"><a href="">Carta</a></li>
+                    <li className="navbar-container-components-items"><a href="">Sucursales</a></li>
+                    <li className="navbar-container-components-items"><a href="">Nosotros</a></li>
+                    <li className="navbar-container-components-items"><a href="">Contacto</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
